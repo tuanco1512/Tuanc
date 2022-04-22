fe42e9bd961310d252f4fba4dede05f1a8e1836c
tuanco1512 <tuanco15122000@gmail.com>
2022-04-22 21:53:35 +0700

    luyen tap js chap 5

---
+++ js chap 05/js/sibling.js
@@ -0,0 +1,6 @@
+var startItem = document.getElementById('two');
+var prevItem = startItem.previousElementSibling;
+var nextItem = startItem.nextSibling;
+
+prevItem.className = 'complete';
+nextItem.className = 'cool';
