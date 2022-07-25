d5f31a21ec64a037bc14e8611d51245328c8a1fe
tuanco1512 <tuanco15122000@gmail.com>
2022-07-25 11:50:17 +0700

    product

--- project-group3-main/assets/js/generate-cake-data.js
+++ project-group3-main/assets/js/generate-cake-data.js
@@ -1,30 +1,45 @@
 $(document).ready(function(){
+    $('.all_list').click(function(){
+        $(this).toggleClass('clicked');
+    });
+});
+$(document).ready(function(){
     $('.breads_show_list').click(function(){
+        $(this).toggleClass('clicked');
         $('.breads_list').toggle('300');
     });
 });
 
 $(document).ready(function(){
     $('.patries_show_list').click(function(){
+        $(this).toggleClass('clicked');
         $('.patries_list').toggle('300');
     });
 });
 
 $(document).ready(function(){
     $('.cookies_show_list').click(function(){
+        $(this).toggleClass('clicked');
         $('.cookies_list').toggle('300');
     });
 });
 
 $(document).ready(function(){
     $('.cupcakes_show_list').click(function(){
+        $(this).toggleClass('clicked');
         $('.cupcakes_list').toggle('300');
     });
 });
 
 $(document).ready(function(){
     $('.coffee_show_list').click(function(){
+        $(this).toggleClass('clicked');
         $('.coffee_list').toggle('300');
+    });
+});
+$(document).ready(function(){
+    $('.merchandise_show_list').click(function(){
+        $(this).toggleClass('clicked');
     });
 });
 
@@ -35,21 +50,66 @@ var categorylist = [
     {id: 4, name:'COOKIES'},
     {id: 5, name:'CUPCAKES'},
     {id: 6, name:'COFFEE'},
-    {id: 7, name:'MERCHANDISE'}
+    {id: 7, name:'MERCHANDISE'},
 ];
 
-var listelement = $('.Pie_list_left ul')
+var itemlist = [
+    {id: 2, name: 'White breads'},
+    {id: 2, name: 'Wheat breads'},
+    {id: 2, name: 'Croissants'},
+    {id: 2, name: 'Breadsticks'},
+    {id: 2, name: 'Buns'},
 
-$.each(categorylist, function(i, data){
-    var newelement = $('.Pie_list_left .item_template').clone();
-    // newelement.removeClass('item_template');
+    {id: 3, name: 'Pies'},
+    {id: 3, name: 'Danishes'},
+    {id: 3, name: 'Macarons'},
+    {id: 3, name: 'Tarts'},
+    {id: 3, name: 'Profiteroles'},
 
-    $(newelement).attr('data-id', data.id);
-    $('a',newelement).text(data.name);
+    {id: 4, name: 'Snickerdoodles'},
+    {id: 4, name: 'Chocolate Chip Cookies'},
+    {id: 4, name: 'Oatmeal Raisin Cookies'},
+    {id: 4, name: 'Shortbread Cookies'},
+    {id: 4, name: 'Whoopie Pies'},
 
-    listelement.append(newelement);
-    newelement.removeClass('item_template');
-});
+    {id: 5, name: 'Standard Cupcakes'},
+    {id: 5, name: 'Cinnamon Cupcake'},
+    {id: 5, name: 'Cookies and Cream Cupcake'},
+    {id: 5, name: 'Key lime Cupcake'},
+
+    {id: 6, name: 'Black Coffee'},
+    {id: 6, name: 'Decaf'},
+    {id: 6, name: 'Espresso'},
+    {id: 6, name: 'Latte'},
+    {id: 6, name: 'Cappuccino'},
+    {id: 6, name: 'Macchiato'},
+    {id: 6, name: 'Americano'},
+];
+
+var listelement = $('.Pie_list_left .categories_list')
+var listitemlement = $('.Pie_list_left .item_list')
+
+// $.each(categorylist, function(i, data){
+//     var newelement = $('.Pie_list_left .category_template').clone();
+
+//     $(newelement).attr('data-id', data.id);
+//     $('.categories_type',newelement).text(data.name);
+
+//     listelement.append(newelement);
+
+//     $.each(itemlist, function(i, item){
+//         if(item.id == data.id){
+//             var newitemelement = $('.Pie_list_left .item_template').clone();
+
+//             $(newitemelement).attr('item-id', item.id);
+//             $('.item_type',newitemelement).text(item.name);
+
+//             listitemlement.append(newitemelement);
+//             newitemelement.removeClass('item_template');
+//         }
+//     });
+//     newelement.removeClass('category_template');
+// });
 
 $(function() {
     var _pageSize = 9;
@@ -70,7 +130,15 @@ $(function() {
         { id: 8, name: 'Cake 8', type: 'Type C', price: 80000, image: 'assets/imgs/slide1.png' },
         { id: 9, name: 'Cake 9', type: 'Type C', price: 90000, image: 'assets/imgs/slide1.png' },
         { id: 10, name: 'Cake 10', type: 'Type D', price: 10000, image: 'assets/imgs/slide1.png' },
-        { id: 11, name: 'Cake 11', type: 'Type D', price: 10000, image: 'assets/imgs/slide1.png' }
+        { id: 11, name: 'Cake 11', type: 'Type D', price: 10000, image: 'assets/imgs/slide1.png' },
+        { id: 12, name: 'Cake 11', type: 'Type D', price: 10000, image: 'assets/imgs/slide1.png' },
+        { id: 13, name: 'Cake 11', type: 'Type D', price: 10000, image: 'assets/imgs/slide1.png' },
+        { id: 14, name: 'Cake 11', type: 'Type D', price: 10000, image: 'assets/imgs/slide1.png' },
+        { id: 15, name: 'Cake 11', type: 'Type D', price: 10000, image: 'assets/imgs/slide1.png' },
+        { id: 16, name: 'Cake 11', type: 'Type D', price: 10000, image: 'assets/imgs/slide1.png' },
+        { id: 17, name: 'Cake 11', type: 'Type D', price: 10000, image: 'assets/imgs/slide1.png' },
+        { id: 18, name: 'Cake 11', type: 'Type D', price: 10000, image: 'assets/imgs/slide1.png' },
+        { id: 19, name: 'Cake 11', type: 'Type D', price: 10000, image: 'assets/imgs/slide1.png' }
     ];
 
     var _tmpElement = $('.P_Img.item_template');
